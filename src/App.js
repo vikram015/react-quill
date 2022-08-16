cac048de9a19f5106baf8d454e8699c1c0d16ab2
vikram <vikram5909015@gmail.com>
2022-08-16 19:17:18 +0530

    fixed issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import ReactQuill,{Quill} from 'react-quill';
 import QuillImageDropAndPaste from 'quill-image-drop-and-paste'
-import { ImageResize } from 'quill-image-resize-module';
+import ImageResize  from 'quill-image-resize-module-react';
 import 'react-quill/dist/quill.snow.css';
 Quill.register('modules/imageDropAndPaste', QuillImageDropAndPaste)
 Quill.register('modules/imageResize',ImageResize)
@@ -10,7 +10,7 @@ function App() {
  
   const [value, setValue] = useState('');
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} modules={{imageDropAndPaste: true,ImageResize: true}} />;
+  return <ReactQuill theme="snow" value={value} onChange={setValue} modules={{imageDropAndPaste: true,imageResize: { parchment: Quill.import('parchment')}}} />;
 }
 
 export default App;
